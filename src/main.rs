aa41090de57005cde8174a2358fd7073804e064e
dannyp99 <dannypires99@gmail.com>
2023-08-18 19:00:00 -0400

    File search working with regex and WalkDir

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use std;
+extern crate walkdir;
+use std::collections::HashSet;
+
+use walkdir::WalkDir;
+use regex::Regex;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 3 {
         println!("{}", args.len());
         println!("Invalid number of args!");
-        println!("Two args required <starting-dir> <search-term>");
+        println!("Two args required <starting-dir> <search-term> <search-type>");
         return;
     }
     let starting_dir: String = args.get(1).unwrap().to_string();
     let search_term: String = args.get(2).unwrap().to_string();
     let temp: &String = &String::from("");
     let search_type: String = args.get(3).unwrap_or_else(|| temp).to_string();
-    if search_type != "dir" || search_type != "file" {
+    println!("search_type: {}", search_type);
+    let valid_set: HashSet<String> = HashSet::from([String::from("dir"), String::from("d"), String::from("file"), String::from("f")]);
+    if !valid_set.contains(&search_type) {
         println!("Invalid search type, please provide arguments \"dir\" or \"file\"");
         return;
     }
@@ -19,9 +26,15 @@ fn main() {
 }
 
 fn search(starting_dir: String, search_term: String, search_type: String) -> () {
-    
+    //get the all items from the current dir
+    let empty_str: &String = &String::from("");
+    let regex = Regex::new(&search_term).unwrap();
+    for file in WalkDir::new(starting_dir).into_iter().filter_map(|file| file.ok()) {
+        if (search_type == "file" || search_type == "f") && regex.is_match(file.file_name().to_str().unwrap_or_else(|| empty_str)) {
+            println!("Found the file: {}", file.path().display());
+        } else if search_type == "dir" || search_type == "d" {
+            println!("path of file: {}", file.path().display());
+        }
+    }
 }
 
-fn get_children_dirs() -> Vec<String> {
- return Vec::new();
-}
