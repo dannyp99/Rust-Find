8a0e565f6ed284992ee21f8fe22b32a7a767ea80
dannyp99 <dannypires99@gmail.com>
2023-08-19 00:37:18 -0400

    Use pattern matching to leverage function call types

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate walkdir;
 
+use std::collections::HashSet;
+
 use walkdir::WalkDir;
 use regex::Regex;
 use clap::Parser;
@@ -9,7 +11,7 @@ use clap::Parser;
 #[clap(author="Danny Pires", version, about="File and Directory Search")]
 struct Search {
     /// The path to start the search default is .
-    starting_path: String,
+    starting_path: Option<String>,
     #[clap(long)]
     /// The pattern to search for
     name: String,
@@ -20,17 +22,24 @@ struct Search {
 
 fn main() {
     let args: Search = Search::parse();
-    let starting_dir: String = args.starting_path; //match args.starting_path {
-    //     Some(value) => value,
-    //     None => String::from(".")
-    // };
+    let starting_dir: Option<String> = args.starting_path;
     let search_term: String = args.name; // Bound search by tearm by start and end
-    let search_type: String  = match args.search_type {
-        Some(value) => value,
+    let search_type_copy = match args.search_type.clone() {
+        Some(x) => x,
         None => String::from("")
     };
-    //TODO: Add pattern matching based on Some()/None to call different versions of search
-    search(starting_dir, search_term, search_type);
+    let valid_types: HashSet<String> = HashSet::from([String::from("d"), String::from("f"), String::from("")]);
+    if !valid_types.contains(&search_type_copy) {
+        println!("-type: {}: unkown type", search_type_copy);
+        return;
+    }
+
+    match (starting_dir, search_term, args.search_type) {
+        (Some(x), y, Some(z)) => search(x, y, z),
+        (Some(x), y, None) => search_all_types(x, y),
+        (None, y, Some(z)) => search(String::from("."), y, z),
+        (None, y, None) => search_all_types(String::from("."), y)
+    }
 }
 
 fn search(starting_dir: String, search_term: String, search_type: String) -> () {
@@ -40,17 +49,32 @@ fn search(starting_dir: String, search_term: String, search_type: String) -> () 
         { String::from("^") + search_term.as_str() }
         else { String::from("^") + search_term.as_str() + &String::from("$")};
     let regex = Regex::new(&regex_search_term).unwrap();
-    dbg!(search_type == "d" || search_type == "dir");
-    dbg!(search_type == "f" || search_type == "file");
     
     for file in WalkDir::new(starting_dir).max_open(4).into_iter().filter_map(|file| file.ok()) {
         let file_path = file.path();
         let file_name = file.file_name().to_str().unwrap_or_else(|| empty_str);
-        if (search_type == "file" || search_type == "f") && file_path.is_file() && regex.is_match(file_name) {
+        if search_type == "f" && file_path.is_file() && regex.is_match(file_name) {
             println!("Found matching File: {}", file_path.display());
-        } else if (search_type == "dir" || search_type == "d")
-            && file_path.is_dir() && file_path.to_str().unwrap().ends_with(&search_term) {
+        } else if file_path.is_dir() && file_path.to_str().unwrap().ends_with(&search_term) {
                 println!("Found matching directory: {}", file_path.display());
+        }
+    }
+}
+
+fn search_all_types(starting_dir: String, search_term: String) {
+    let empty_str: &String = &String::from("");
+    // Enforce ending if there is no wildcard match
+    let regex_search_term =  if search_term.chars().nth_back(0).eq(&Option::Some('*')) 
+        { String::from("^") + search_term.as_str() }
+        else { String::from("^") + search_term.as_str() + &String::from("$")};
+    let regex = Regex::new(&regex_search_term).unwrap();
+    for file in WalkDir::new(starting_dir).max_open(4).into_iter().filter_map(|file| file.ok()) {
+        let file_path = file.path();
+        let file_name = file.file_name().to_str().unwrap_or_else(|| empty_str);
+        if file_path.is_file() && regex.is_match(file_name) {
+            println!("Found matching File: {}", file_path.display());
+        } else if file_path.is_dir() && file_path.to_str().unwrap().ends_with(&search_term) {
+            println!("Found matching directory: {}", file_path.display());
         }
     }
 }
