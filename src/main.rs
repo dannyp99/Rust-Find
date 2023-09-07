8a8be1f170321f7e6fa2cef2a94b014d115e06fa
dannyp99 <dannypires99@gmail.com>
2023-09-07 17:13:19 -0500

    Format changes

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ extern crate walkdir;
 
 use std::{collections::HashSet, path::Path};
 
-use walkdir::WalkDir;
-use regex::Regex;
 use clap::Parser;
+use regex::Regex;
+use walkdir::WalkDir;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
-#[clap(author="Danny Pires", version, about="File and Directory Search")]
+#[clap(author = "Danny Pires", version, about = "File and Directory Search")]
 struct Search {
     /// The path to start the search default is .
     starting_path: Option<String>,
     #[clap(long)]
     /// The pattern to search for
     name: String,
-    #[clap(long="type")]
+    #[clap(long = "type")]
     /// The type of search
-    search_type: Option<String>
+    search_type: Option<String>,
 }
 
 fn main() {
@@ -26,9 +26,10 @@ fn main() {
     let search_term: String = args.name; // Bound search by tearm by start and end
     let search_type_copy: String = match args.search_type.clone() {
         Some(x) => x,
-        None => String::from("")
+        None => String::from(""),
     };
-    let valid_types: HashSet<String> = HashSet::from([String::from("d"), String::from("f"), String::from("")]);
+    let valid_types: HashSet<String> =
+        HashSet::from([String::from("d"), String::from("f"), String::from("")]);
     if !valid_types.contains(&search_type_copy) {
         println!("-type: {}: unkown type", search_type_copy);
         return;
@@ -38,25 +39,31 @@ fn main() {
         (Some(x), y, Some(z)) => search(x, y, z),
         (Some(x), y, None) => search_all_types(x, y),
         (None, y, Some(z)) => search(String::from("."), y, z),
-        (None, y, None) => search_all_types(String::from("."), y)
+        (None, y, None) => search_all_types(String::from("."), y),
     }
 }
 
 fn search(starting_dir: String, search_term: String, search_type: String) -> () {
     let empty_str: &String = &String::from("");
     // Enforce ending if there is no wildcard match
-    let regex_search_term: String = if search_term.chars().nth_back(0).eq(&Option::Some('*')) 
-        { String::from("^") + search_term.as_str() }
-        else { String::from("^") + search_term.as_str() + &String::from("$")};
+    let regex_search_term: String = if search_term.chars().nth_back(0).eq(&Option::Some('*')) {
+        String::from("^") + search_term.as_str()
+    } else {
+        String::from("^") + search_term.as_str() + &String::from("$")
+    };
     let regex: Regex = Regex::new(&regex_search_term).unwrap();
-    
-    for file in WalkDir::new(starting_dir).max_open(4).into_iter().filter_map(|file| file.ok()) {
+
+    for file in WalkDir::new(starting_dir)
+        .max_open(4)
+        .into_iter()
+        .filter_map(|file| file.ok())
+    {
         let file_path: &Path = file.path();
         let file_name: &str = file.file_name().to_str().unwrap_or_else(|| empty_str);
         if search_type == "f" && file_path.is_file() && regex.is_match(file_name) {
             println!("Found matching File: {}", file_path.display());
         } else if file_path.is_dir() && file_path.to_str().unwrap().ends_with(&search_term) {
-                println!("Found matching directory: {}", file_path.display());
+            println!("Found matching directory: {}", file_path.display());
         }
     }
 }
@@ -64,11 +71,17 @@ fn search(starting_dir: String, search_term: String, search_type: String) -> () 
 fn search_all_types(starting_dir: String, search_term: String) {
     let empty_str: &String = &String::from("");
     // Enforce ending if there is no wildcard match
-    let regex_search_term: String = if search_term.chars().nth_back(0).eq(&Option::Some('*')) 
-        { String::from("^") + search_term.as_str() }
-        else { String::from("^") + search_term.as_str() + &String::from("$")};
+    let regex_search_term: String = if search_term.chars().nth_back(0).eq(&Option::Some('*')) {
+        String::from("^") + search_term.as_str()
+    } else {
+        String::from("^") + search_term.as_str() + &String::from("$")
+    };
     let regex: Regex = Regex::new(&regex_search_term).unwrap();
-    for file in WalkDir::new(starting_dir).max_open(4).into_iter().filter_map(|file: Result<walkdir::DirEntry, walkdir::Error>| file.ok()) {
+    for file in WalkDir::new(starting_dir)
+        .max_open(4)
+        .into_iter()
+        .filter_map(|file: Result<walkdir::DirEntry, walkdir::Error>| file.ok())
+    {
         let file_path: &Path = file.path();
         let file_name: &str = file.file_name().to_str().unwrap_or_else(|| empty_str);
         if file_path.is_file() && regex.is_match(file_name) {
@@ -78,4 +91,3 @@ fn search_all_types(starting_dir: String, search_term: String) {
         }
     }
 }
-
